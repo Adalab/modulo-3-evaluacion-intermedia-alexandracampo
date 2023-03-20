40f4f257301ee56c74851834139a87c293e9dd81
Alexandra Campo <alexandracampo86@gmail.com>
2023-03-20 12:08:28 +0100

    add issues

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
 // Fichero src/components/App.js
 import '../styles/App.css';
 import data from '../data/data.json';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 
 function App() {
 
   const [search, setSearch] = useState('');
   const [select, setSelect] = useState('');
+  const [data, setData] = useState([]);
+  const [newQuote, setNewQuote] = useState('');
+  const [newCharacter, setNewCharacter] = useState('');
+
+
+
+  //UseEffect donde controlamos el fecth a la API
+  useEffect(() => {
+
+    fetch('https://beta.adalab.es/curso-intensivo-fullstack-recursos/apis/quotes-friends-tv-v1/quotes.json')
+      .then(response => response.json())
+      .then(dataApi => {
+        setData(dataApi);
+      });
+
+  }, []);
 
 
   const renderList = () => {
@@ -26,6 +42,7 @@ function App() {
           <hr></hr>
         </li>
       ))
+      .reverse();
   }
 
   const handleFilterInput = (ev) => {
@@ -40,24 +57,56 @@ function App() {
     ev.preventDefault();
   }
 
+  const handleInputNewQuote = (ev) => {
+    setNewQuote(ev.target.value);
+  }
+
+  const handleInputNewCharacter = (ev) => {
+    setNewCharacter(ev.target.value);
+  }
+
+  const handleAddQuote = (ev) => {
+    //Validar que los inputs tengan contenido o no:
+    if (newQuote && newCharacter) {
+      data.push({
+        "quote": newQuote,
+        "character": newCharacter
+      })
+      setData([...data])
+      setNewQuote('')
+      setNewCharacter('')
+    }
+  }
+
   return (
     <div>
       <h1>Frases de friends</h1>
-
       <form onSubmit={handleSubmit}>
-        <label htmlFor="quote">Filtrar por frase:</label>
-        <input onChange={handleFilterInput} type="text" name="quote" />
 
-        <label htmlFor="character">Filtrar por personaje:</label>
-        <select name="character" onChange={handleFilterCharacter}>
-          <option value="">Todos</option>
-          <option>Ross</option>
-          <option>Monica</option>
-          <option>Joey</option>
-          <option>Phoebe</option>
-          <option>Chandler</option>
-          <option>Rachel</option>
-        </select>
+        <fieldset className="fieldsetSearch">
+          <legend>Buscar:</legend>
+          <label htmlFor="quote">Filtrar por frase:</label>
+          <input onChange={handleFilterInput} type="text" name="quote" />
+          <label htmlFor="character">Filtrar por personaje:</label>
+          <select name="character" onChange={handleFilterCharacter}>
+            <option value="">Todos</option>
+            <option>Ross</option>
+            <option>Monica</option>
+            <option>Joey</option>
+            <option>Phoebe</option>
+            <option>Chandler</option>
+            <option>Rachel</option>
+          </select>
+        </fieldset>
+
+        <fieldset className="fieldsetAdd">
+          <legend>Añadir una nueva frase</legend>
+          <label>Frase:</label>
+          <input value={newQuote} onInput={handleInputNewQuote} type="text" placeholder='Añade frase...'></input>
+          <label>Personaje:</label>
+          <input value={newCharacter} onInput={handleInputNewCharacter} type="text" placeholder='Quién lo dijo...'></input>
+          <button onClick={handleAddQuote}>Añadir nueva frase</button>
+        </fieldset>
       </form>
 
       <ul>{renderList()}</ul>
